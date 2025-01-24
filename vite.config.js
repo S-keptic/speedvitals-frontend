dd1a11a07a546ff2b53b98d26be35dd411994470
Anmol <anmol.volt@gmail.com>
2025-01-25 00:04:32 +0530

    x

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
